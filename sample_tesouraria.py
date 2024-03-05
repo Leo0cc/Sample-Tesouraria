29aa91d11e77648ee7ac43e66535d70761b935c6
Leo0cc <leo0c.costaa@gmail.com>
2024-03-05 15:23:10 -0300

    quase

--- sample_tesouraria.py
+++ sample_tesouraria.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 #Funcoes que serao ultilizadas
 st.set_page_config(layout="wide")
-
+#Funcoao para calculo do YTM
 def bond_ytm(price, par, T, coup, freq=2, guess=0.05):
     freq = float(freq)
     periods = T*freq
@@ -18,7 +18,7 @@ def bond_ytm(price, par, T, coup, freq=2, guess=0.05):
         
     return optimize.newton(ytm_func, guess)
 
-
+#Funcao para calculo do valor presente
 def bond_price(par, T, ytm, coup, freq=2):
     freq = float(freq)
     periods = T*freq
@@ -28,19 +28,24 @@ def bond_price(par, T, ytm, coup, freq=2):
             par/(1+ytm/freq)**(freq*T)
     return price
 
+#Funcao para calculo da Duration de Macaulay
+def bond_macaulay_d(par,ytm, coup, T):
+    periods = T * 2
+    coupon_rate= (coup/100)/2
+    ytm_semi = ytm/2
+    pv = bond_price(par,T,ytm, coup)
+    pvc = par*coupon_rate*((1-(1/(1+ytm_semi)**periods))/ytm_semi)
+    H = pvc/pv
+    
+    
+    dura = (( 1 + ytm_semi ) / ytm_semi ) * H + (( ytm_semi - coupon_rate ) / ytm_semi) * periods * (1-H)
+    return dura/2 
 
 
-def bond_macaulay_d():
-    pvcf = []
-    for i in range(1, 10):
-        pvcf.append("oi")
-    return pvcf
 
 
 
-
-
-
+#Funcao para calculo da Convexidade
 def bond_convexity(price, par, T, coup, freq=2, dy=0.01):
     ytm = bond_ytm(price, par, T, coup, freq)
 
@@ -53,6 +58,8 @@ def bond_convexity(price, par, T, coup, freq=2, dy=0.01):
     convexity = (price_minus+price_plus-2*price)/(price*dy**2)
     return convexity
 
+
+#Funcao para calculo da Duration Efetiva
 def effective_duration(price, par, T, coup, freq=2, dy=0.01):
     ytm = bond_ytm(price, par, T, coup)
     
@@ -65,7 +72,7 @@ def effective_duration(price, par, T, coup, freq=2, dy=0.01):
     mduration = (price_minus-price_plus)/(2*price*dy)
     return mduration
 
-
+#Definicao dos parametros para criacao dos titulos
 class bonds:
     def __init__(self):
         self.par_value = 1000
@@ -83,9 +90,10 @@ class bonds:
                 break
         
         self.pv = bond_price(self.par_value, self.vencimento,self.ytm_init , self.coupon)
+        self.macaulay = bond_macaulay_d(self.par_value,self.ytm_init, self.coupon, self.vencimento)
         self.efecdu = effective_duration(self.pv, self.par_value, self.vencimento, self.coupon)
         self.convex = bond_convexity(self.pv, self.par_value,self.vencimento ,self.coupon )
-        
+#Definindo diferentes spreads e niveis de risco
     def risco_buy(self):
         teste = np.random.randint(low=0, high= 3)
         if teste == 0:
@@ -104,7 +112,7 @@ class bonds:
         elif teste == 2:
             self.ytm_init = self.ytm_init - np.random.uniform(low=self.ytm_init*0.2, high= self.ytm_init*0.3)
         
-
+#Gerando o DataFrame para carteira de titulos ativos
 def create_bonds_buy():
     data = []
     for i in range(len(cdi["CDI Futuro"])):
@@ -117,11 +125,14 @@ def create_bonds_buy():
         cupom = round(bond.coupon, 2)
         effec = (bond.efecdu)
         convex = (bond.convex)
-        data.append({"Nome": title, "Valor Presente": pv, "YTM" : ytm, "Vencimento" : vencimento, "Taxa de Cupom" : cupom, "Duration Efetiva": effec, "Convexidade":convex})
+        duration = (bond.macaulay)
+        data.append({"Nome": title, "Valor Presente": pv, "YTM" : ytm, "Vencimento" : vencimento, "Taxa de Cupom" : cupom, "Duration Efetiva": effec, "Convexidade":convex, "Duration": duration})
     df = pd.DataFrame(data)
     df.set_index("Nome", inplace=True)
     return df.sort_values(by="Vencimento", ascending=True)
 
+
+#Gerando o DataFrame para carteira de titulos passivos
 def create_bonds_sell():
     data = []
     for i in range(len(cdi["CDI Futuro"])):
@@ -134,13 +145,22 @@ def create_bonds_sell():
         cupom = (bond.coupon)
         effec = (bond.efecdu)
         convex = (bond.convex)
-        data.append({"Nome": title, "Valor Presente": pv, "YTM" : ytm, "Vencimento" : vencimento, "Taxa de Cupom" : cupom, "Duration Efetiva": effec, "Convexidade":convex})
+        duration = (bond.macaulay)
+        data.append({"Nome": title, "Valor Presente": pv, "YTM" : ytm, "Vencimento" : vencimento, "Taxa de Cupom" : cupom, "Duration Efetiva": effec, "Convexidade":convex, "Duration": duration})
     df = pd.DataFrame(data)
     df.set_index("Nome", inplace=True)
     return df.sort_values(by="Vencimento", ascending=True)
 
+#Contabilizando a posicao total da carteira
+
+def  position_total(assets):
+    carteira = []
+    pv = assets["Valor Presente"].sum()
+    
+    return 
 
 
+#Formatacao dos dados no Streamlit
 st.title("Simulação de Titulos")
 
 #Geracao das taxas futuras ultilizadas na criacao dos titulos
@@ -167,11 +187,21 @@ st.subheader("Curva de CDI Futuro")
 
 st.line_chart(cdi.set_index('Ano'), y="CDI Futuro")
 
-#Geracao de titulos a partir da curva de juros, com diferentes niveis de risco
+#Geracao de titulos a partir da curva de juros
 
 
 assets = create_bonds_buy()
+assets_portfolio = position_total(assets)
+
 liabilities = create_bonds_sell()
+liabilities_portfolio = position_total(liabilities)
+
+def  position_total(assets):
+    carteira = []
+    pv = assets["Valor Presente"].sum()
+    return pv
+
+st.write(position_total(assets))
 
 col1, col2 = st.columns(2)
 with col1:
